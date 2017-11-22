fcbc92ea66be1aa89539232ec3659622a54e2588
wq <z464337866@163.com>
2017-11-22 16:42:42 +0800

    第一次修改!

---
+++ ChianNo1/src/cn/bank/ChinaNo1.java
@@ -0,0 +1,9 @@
+package cn.bank;
+
+public class ChinaNo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("China是NO1");
+	}
+}
--- HelloWorld/src/cn/bank/main/HelloWorld.java
+++ HelloWorld/src/cn/bank/main/HelloWorld.java
@@ -3,6 +3,6 @@ package cn.bank.main;
 public class HelloWorld {
 
 	 public static void main(String[] args) {
-		System.out.println("这是一个新的工程!  我随便瞎改一下!");
+		System.out.println("这是一个新的工程!  我随便瞎改一下!  我想试试如何修改别人的代码!");
 	}
 }
